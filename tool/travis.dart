d2fb4c852a9a2e849afb93eb7c40dafe4e884967
alex <alex@tekartik.com>
2020-05-01 00:25:15 +0200

    test: dart 2.9 formatting

--- tool/travis.dart
+++ tool/travis.dart
@@ -6,15 +6,16 @@ Future main() async {
 
   // Formatting change int 2.9 with hashbang first line
   if (dartVersion >= Version(2, 9, 0, pre: '0')) {
-    await shell.run('dartfmt -n --set-exit-if-changed .');
+    await shell.run('''
+    # Formatting
+    dartfmt -n --set-exit-if-changed .
+    ''');
   }
 
   await shell.run('''
   # Analyze code
   dartanalyzer --fatal-warnings --fatal-infos .
-  dartfmt -n --set-exit-if-changed .
-
+  # Test
   pub run test -p vm
-  
   ''');
 }
